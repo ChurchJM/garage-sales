e72f6eda54f28f9d86e689130c29fb4412ce3b55
churchjm <jeffrey.m.church@gmail.com>
2026-08-08 20:38:43 -0700

    Created solution and added front-end web project.

---
+++ src/GarageSales.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GarageSales.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
